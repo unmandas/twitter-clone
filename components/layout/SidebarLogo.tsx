629d075138fca7246afebf30b41c4fbbf6441981
unmandas <unmandas1998@gmail.com>
2023-10-05 17:56:10 +0530

    Initial commit

---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { FaXTwitter } from 'react-icons/fa6'
+
+
+const SidebarLogo = () => {
+    const router = useRouter();
+    return (
+        <div className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer-transition">
+            <FaXTwitter size={28} color="white" />
+        </div>
+    )
+}
+
+export default SidebarLogo
