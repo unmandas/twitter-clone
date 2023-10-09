f6887868b72fcb0f26ee691093812ff7e6c0ee0b
unmandas <unmandas1998@gmail.com>
2023-10-09 19:53:26 +0530

    Added login and register modal

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -5,7 +5,10 @@ import { FaXTwitter } from 'react-icons/fa6'
 const SidebarLogo = () => {
     const router = useRouter();
     return (
-        <div className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer-transition">
+        <div
+
+            onClick={() => router.push('/')}
+            className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer-transition">
             <FaXTwitter size={28} color="white" />
         </div>
     )
--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,10 +1,17 @@
+import useLoginModal from "@/hooks/useLoginModal";
 import { useRouter } from "next/router"
+import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa"
 const SidebarTweetButton = () => {
     const router = useRouter();
+    const loginModal = useLoginModal();
+
+    const onClick = useCallback(() => {
+        loginModal.onOpen();
+    }, [loginModal])
 
     return (
-        <div onClick={() => router.push('/')}>
+        <div onClick={onClick}>
             {/* Mobile view */}
             <div
                 className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer"
