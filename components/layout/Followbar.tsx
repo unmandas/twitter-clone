d08d0b7090bae2ca6e5f15c27d4f342fe54b19c9
unmandas <unmandas1998@gmail.com>
2023-10-18 11:22:46 +0530

    added avatar

--- components/layout/Followbar.tsx
+++ components/layout/Followbar.tsx
@@ -1,6 +1,13 @@
+import useUsers from '@/hooks/useUsers'
 import React from 'react'
+import Avatar from '../Avatar';
 
 const Followbar = () => {
+    const { data: users = [] } = useUsers();
+    if (users.length === 0) {
+        return null;
+    }
+
     return (
         <div className="px-6 py-4 hidden lg:block">
             <div className="bg-neutral-800 rounded-xl p-4">
@@ -8,7 +15,17 @@ const Followbar = () => {
                     Who to follow
                 </h2>
                 <div className='flex flex-col gap-6 mt-4'>
-                    {/* TODO USER LIST */}
+                    {users.map((user: Record<string, any>) => (
+                        <div key={user.id} className='flex flex-row gap-4'>
+                            <Avatar userId={user.id} />
+                            <div className='flex flex-col'>
+                                <p className='text-white font-semibold text-sm'>{user.name}</p>
+                                <p className='text-neutral-400 text-sm'>
+                                    @{user.username}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
